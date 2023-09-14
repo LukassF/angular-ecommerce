d8cad490674c2f4810b0d5482be792a511bfb98a
Luks245 <florekl03c@lo5.bielsko.pl>
2023-09-14 10:28:49 +0200

    added loading indicators

--- frontend/src/app/pages/home/components/arrivals/arrivals.component.ts
+++ frontend/src/app/pages/home/components/arrivals/arrivals.component.ts
@@ -1,9 +1,15 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
 
 @Component({
   selector: "app-arrivals",
   templateUrl: "./arrivals.component.html",
 })
-export class ArrivalsComponent {
+export class ArrivalsComponent implements OnChanges {
+  isLoading: boolean = true;
   @Input() fetchedProducts: Product[] | undefined;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    //@ts-ignore
+    if (changes.fetchedProducts.currentValue) this.isLoading = false;
+  }
 }
--- frontend/src/app/pages/home/components/featured/featured.component.ts
+++ frontend/src/app/pages/home/components/featured/featured.component.ts
@@ -1,9 +1,15 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
 
 @Component({
   selector: "app-featured",
   templateUrl: "./featured.component.html",
 })
-export class FeaturedComponent {
+export class FeaturedComponent implements OnChanges {
+  isLoading: boolean = true;
   @Input() fetchedProducts: Product[] | undefined;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    //@ts-ignore
+    if (changes.fetchedProducts.currentValue) this.isLoading = false;
+  }
 }
