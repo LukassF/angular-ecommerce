fb798e958bf709dbee450c72c31e920b6fc407fd
Luks245 <florekl03c@lo5.bielsko.pl>
2023-10-11 12:24:27 +0200

    bypassing popup window blocking

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -8,6 +8,15 @@ export class AppComponent implements OnInit {
   title = "eCommerce";
 
   ngOnInit(): void {
+    // window.open("https://ngecommerce.onrender.com/api/clothes", "_blank");
+  }
+  openServer() {
     window.open("https://ngecommerce.onrender.com/api/clothes", "_blank");
+    if (document.getElementById("alert")) {
+      document.getElementById("alert")!.style.opacity = "0";
+      setTimeout(() => {
+        document.getElementById("alert")!.style.display = "none";
+      }, 500);
+    }
   }
 }
